29a0c2c145ce8d9d9dccb830ac70cbdf85754a67
TomoyaIzumori <101162831+Tomoya-Izumori@users.noreply.github.com>
2024-01-22 10:23:43 +0900

    微調整

--- template/sns.php
+++ template/sns.php
@@ -4,6 +4,10 @@
       <h2 class="section__header--h2">SNS</h2>
       <span class="section__header--h2--ja">instagram</span>
     </div>
-
+    <div class="section__contents">
+      <div class="p-sns">
+        
+      </div>
+    </div>
   </div>
 </section>
